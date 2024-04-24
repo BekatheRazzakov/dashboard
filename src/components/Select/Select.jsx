3597580757d17fa01600a0fd35673d442659b189
Bektur Razzakov <brzzkv@gmail.com>
2024-04-24 20:26:07 +0600

    Select component

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ReactComponent as ArrowDown} from "../../assets/arrow-down.svg";
+import './select.css';
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {setDropdown} from "../../features/usersSlice";
+
+const Select = ({name, value, options, changeHandler, style}) => {
+  const dispatch = useAppDispatch();
+  const currentDropdown = useAppSelector(state => state.userState.currentDropDown);
+
+  return (
+    <div
+      className={`select ${currentDropdown === name ? 'select-open' : ''}`}
+      onClick={(e) => {
+        e.stopPropagation();
+        dispatch(setDropdown(name));
+      }}
+      style={style}
+    >
+      <div className="select-current-option">
+        {value}<ArrowDown/>
+      </div>
+      <div className="select-options">
+        {
+          options.filter(option => option !== (value || '')).map(option => (
+            <span
+              className="select-option"
+              key={option}
+              onClick={() => {
+                changeHandler(option);
+                setTimeout(() => dispatch(setDropdown('')), 10);
+              }}>{option}</span>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Select;
